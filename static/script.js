672f37cc78adb8877b2947a36583876da6ee479d
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-04-08 10:34:51 +0600

    add object Post

--- static/script.js
+++ static/script.js
@@ -3,3 +3,11 @@ const User = {
     id: 1,
     email: 'mike@gmail.com',
     autorized:true};
+
+// Создан объект класса Постов. я решила связать user  и  post просто указав ссылку на сам объект, путем его объявления.
+// Так как он будет в будущем ссылатся именно на данного пользователя.
+const Post={
+    id:1,
+    author:User,
+    image:'horror.jpg',
+    description:'Hello'};
